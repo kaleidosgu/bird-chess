fde235f6bff1f830105ca5e1757df120f1b66b96
wrw9@163.com <wrw9@163.com@458bd12f-e22f-cdd9-0cbd-e4c0bc0b538c>
2011-11-24 21:53:54 +0000

    lua修改

--- trunk/out/Res/UILua/UIGlobalInit.lua
+++ trunk/out/Res/UILua/UIGlobalInit.lua
@@ -2,15 +2,17 @@ local pDesk = basewnd.toObject(g_UIGlobal["ptDesk"],"CWndBase")
 local ptMainClient = CWndBase:new();
 ptMainClient:Create(0,0,800,600,pDesk,0);
 g_UIGlobal["ptMainClient"] = ptMainClient
-
-
+g_CMDList = {"","","","","","","","","","","","","","","","",""}
+g_CurCmd = 1
+g_MaxCmd = 15
 --初始化正弦，余弦值
 g_CosAngle = {}
 g_SinAngle = {}
 
+
 for i = 1, 361 do
 	local theta = (i - 1) * math.pi / 180
-	g_CosAngle[i] = math.cos(theta) 
-	g_SinAngle[i] = math.sin(theta) 
+	g_CosAngle[i] = math.cos(theta)
+	g_SinAngle[i] = math.sin(theta)
 	--print( tostring(g_CosAngle[i] ) .. "==" .. tostring(g_SinAngle[i]) )
 end
--- trunk/out/Res/UILua/chatbird.lua
+++ trunk/out/Res/UILua/chatbird.lua
@@ -17,15 +17,15 @@ local function OnMessage(self,nUIEvent,nDlgID)
 				self.pWndBirdChat:ShowWindow(true)
 				keyres = WND_RESULT_YES
 				consoleAddText("true")
-				
+
 				-- local bseEdit = basewnd.toObject(self.stEdit,"CWndBase")
-				self.stEdit:SetFocus(self.stEdit)
-				self.stEdit:SetText("")
+				--self.stEdit:SetFocus(self.stEdit)
+				--self.stEdit:SetText("")
 			end
 		end
 	elseif nUIEvent == WND_ONCHAR then
 		if nDlgID == 333 then
-			SendChatMsg(self.stEdit:GetText())
+--~ 			SendChatMsg(self.stEdit:GetText())
 			self.pWndBirdChat:ShowWindow(false)
 			self.fly = (not self.fly)
 			keyres = WND_RESULT_YES
@@ -39,25 +39,34 @@ local function OnUpdate(self,e)
 	if self.fly == true then
 		self.tickCount = self.tickCount + e
 		if self.tickCount > 0.01 then
-			consoleClear()
+			--consoleClear()
 			self.tickCount = self.tickCount - 0.01
 			local x = self.ptReturn:GetClientPos().x
 			local y = 0
-			x = x + 1
+			local nRandy = 1
+			x = x - 3
 			local idx = 0
-			if x > 1000 then
-				x = 1
+			if x < 1 then
+				x = 2000
+
+				if self.stEdit:GetText()~= "" then
+					SendChatMsg(self.stEdit:GetText())
+					self.stEdit:SetFocus(self.stEdit)
+					self.stEdit:SetText("")
+				end
 			end
 			if x > 361 then
 				idx = x % 361
 				if idx == 0 then
+					nRandy = math.random(1,5)
 					idx = 1
 				end
 			else
 				idx = x
 			end
-			y = g_SinAngle[idx] * 50
-			self.ptReturn:MoveWindow( x ,self.bsey + y)
+			y = g_SinAngle[idx] *5*nRandy
+			--self.ptReturn:MoveWindow( x ,self.bsey + y)
+			self.ptReturn:MoveWindow( x ,700 + y)
 		end
 	end
 end
@@ -69,28 +78,29 @@ local function regEvent( parent )
 end
 local function createBird( ptReturn )
 	local pChatBird = CWndButton:new();
-	pChatBird:Create(0,0,0,0,"res/pic/EntityPic.png",ptReturn,115);		
+	pChatBird:Create(0,0,0,0,"res/pic/EntityPic.png",ptReturn,115);
 	pChatBird:AddRes(0,378,33,33)
 	pChatBird:AddRes(0,410,33,33)
 	pChatBird:AddRes(0,505,33,33)
 	pChatBird:AddRes(0,543,33,33)
-	
+
 	ptReturn.ptReturn = ptReturn
 	ptReturn.tickCount = 0
 	ptReturn.bsey = ptReturn:GetClientPos().y
 	regEvent(ptReturn)
 	ptReturn.fly = true
-	
+
 	local pWndBirdChat = CWndBase:new()
 	pWndBirdChat:Create(30,0,0,0,ptReturn,0)
 	-- pWndBirdChat:ShowWindow(false)
 	ptReturn.pWndBirdChat = pWndBirdChat
-	
-	for i = 1, 3 do
+
+	for i = 1, 1 do
 		local sbg = CWndLoadPicture:new()
-		sbg:Create( i * 30 - 30, -15, "res/pic/EntityPic.png", pWndBirdChat, 0, true, 868,605,86,73 )
+		--sbg:Create( i * 30 - 30, -15, "res/pic/EntityPic.png", pWndBirdChat, 0, true, 868,605,86,73 )
+		sbg:Create( 15 , 0, "res/pic/EntityPic.png", pWndBirdChat, 0, true, 40,540,60,30 )
 	end
-	
+
 	local stEdit = CWndEdit:new();
 	stEdit:CreateNoFont(15,0,200,28,pWndBirdChat,333)
 	ptReturn.stEdit = stEdit
